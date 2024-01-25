7d479db96f186d368738a626f672d260cad61aa0
unknown <jitbiswas485@gmail.com>
2024-01-25 10:24:21 +0530

    Initial Commit

---
+++ array.js
@@ -0,0 +1,49 @@
+let fruits = ["Apple","Banana","Orange","Grape"];
+
+// console.log(fruits[0]);
+
+// // for(let fruit in fruits){
+// //     console.log(fruits[fruit]);
+// // }
+
+// //---------------------array methods-----------------------
+
+// // push
+// fruits.push("Guava");
+// console.log(fruits);
+
+// //pop
+// fruits.pop()
+// console.log(fruits);
+
+// //unshift
+// fruits.unshift("Kiwi");
+// console.log(fruits);
+
+// //shift
+// fruits.shift()
+// console.log(fruits);
+
+// //indexof-------to find the index of an element
+// let index = fruits.indexOf("Banana");
+// console.log(index);
+
+
+// for(let i=0; i<=fruits.length; i++){
+//     console.log(fruits[i]);
+// }
+
+
+// // slicing an array
+
+// let citrus = fruits.slice(1,3);
+// console.log(citrus);
+
+// let remove = fruits.splice(1,2,"Apple","Banana");
+// console.log(fruits);
+
+// let more =["Pineapple","Grapefruit"];
+// let all = fruits.concat(more);
+// console.log(all);
+
+
